41b1042e5004b45503421e06852f1c8fc330c6c8
aalteirac <anthony@alteirac.com>
2022-10-31 18:04:32 +0100

    change default

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ segTarget=st.multiselect("Segment Target:",segment, default=["Champions","Hibern
 if len(segTarget)>0: 
     c,c2=st.columns(2)
     discount=c.slider("Discount on Target Segment:",min_value=0,max_value=50,step=5,value=5)
-    increase=c2.slider("Anticipated Sales Increase on Target Segment:",min_value=0,max_value=50,value=10)
+    increase=c2.slider("Anticipated Sales Increase on Target Segment:",min_value=0,max_value=50,value=20)
     df2=getRevSplit(segTarget,discount,increase)
     dfAll=df2.loc[df2['TYPE'].isin(['ALL','EXCEPT'])] 
     dfAll=dfAll.groupby('PR',as_index=False).sum()
